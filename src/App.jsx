28362c1e762376d87926dc39721457494eb975b1
ldanh270 <ducanhle.dn@gmail.com>
2025-07-05 16:18:54 +0700

    install scss/sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,12 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import rectsvg from '~/assets/react.svg';
-import './App.css';
+import * as styles from './App.module.scss';
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
-        <>
+        <div className={styles['App']}>
             <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={rectsvg} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
+                <span>Day la text</span>
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </>
+        </div>
     );
 }
 
