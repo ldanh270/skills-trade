c21e1ea94a957c7180bcdfc96a0e933b60568f24
ldanh270 <ducanhle.dn@gmail.com>
2025-07-09 15:05:30 +0700

    Config auto sort importing

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [
-		react(), // Vite sẽ tự tìm .babelrc
-	],
-	resolve: {
-		alias: {
-			'~': '/src',
-		},
-	},
+    plugins: [
+        react(), // Vite sẽ tự tìm .babelrc
+    ],
+    resolve: {
+        alias: {
+            '~': '/src',
+        },
+    },
 })
