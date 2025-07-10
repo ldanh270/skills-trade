133d5e21d6941397b2c2a191a3ac8ac121c68f1e
ldanh270 <ducanhle.dn@gmail.com>
2025-07-10 23:16:04 +0700

    Settup mock db + gitignore

---
+++ src/api/posts.js
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const BASE_URL = `http://localhost:3001`
+
+const fetchPosts = async (page = 1, limit = 10) => {
+    const response = await axios.get(
+        `${BASE_URL}/posts?_page=${page}&_limit=${limit}&_sort=createdAt&_order=desc`,
+    )
+    return response.data
+}
+
+export default fetchPosts
