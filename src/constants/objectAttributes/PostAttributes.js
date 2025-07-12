34149cc0abc30c66649d1de0cd3c5976a5971728
ldanh270 <ducanhle.dn@gmail.com>
2025-07-13 00:05:46 +0700

    PostAttributes: Clean code

--- src/constants/objectAttributes/PostAttributes.js
+++ src/constants/objectAttributes/PostAttributes.js
@@ -1,10 +1,12 @@
+// Post Types used in filtering or creating posts
 export const POST_TYPE = {
-    Offer: 'Offer',
-    Hire: 'Hire',
+    Offer: 'Offer', // Someone offering help/skills
+    Hire: 'Hire', // Someone looking to hire or find help
 }
 
+// Post Reactions used in likes/dislikes
 export const POST_REACTION = {
-    LIKE: 'Like',
-    DISLIKE: 'Dislike',
-    NONE: 'None',
+    LIKE: 'Like', // User liked the post
+    DISLIKE: 'Dislike', // User disliked the post
+    NONE: 'None', // No reaction yet
 }
