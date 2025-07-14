c973478c3247e8a9ecc1623bdfd683f01133f4c6
ldanh270 <ducanhle.dn@gmail.com>
2025-07-14 22:58:11 +0700

    Sidebar: Done UX UI

---
+++ src/pages/Upload/SideBar/SideBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import styles from './Sidebar.module.scss'
+
+const Sidebar = ({ steps, current, status }) => {
+    return (
+        <div className={styles['Sidebar']}>
+            {steps.map((step, idx) => (
+                <div key={step.key} className={styles['step-item']}>
+                    <div
+                        className={`${styles['step-circle']} ${
+                            current >= idx ? styles['active'] : ''
+                        }`}
+                    >
+                        {status === 'success' && idx === steps.length - 1
+                            ? '✔'
+                            : current > idx
+                              ? '✓'
+                              : idx + 1}
+                    </div>
+                    <span className={styles['step-label']}>{step.label}</span>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Sidebar
