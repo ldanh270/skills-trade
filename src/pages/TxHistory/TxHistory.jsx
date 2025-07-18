7dbb809a59044c50456fc82a96c8a37a4f63ea70
Hoangphuong2k5 <phuongnguyen200504@gmail.com>
2025-07-18 19:07:54 +0700

    check lại profile

---
+++ src/pages/TxHistory/TxHistory.jsx
@@ -0,0 +1,11 @@
+import * as styles from './TxHistory.module.scss'
+
+function TxHistory() {
+    return (
+        <div className={styles['TxHistory']}>
+            <h1>Chat page</h1>
+        </div>
+    )
+}
+
+export default TxHistory
